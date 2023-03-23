77f3b6f6dcc31e18d423edc270d667262106ba8a
philiporlando <phlp.orlando@gmail.com>
2023-03-22 23:40:42 -0700

    Add try/except, progress, input val, overwrite

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # fgdb_to_gpkg
 
-`fgdb_to_gpkg` is a Python package that converts all feature classes within a specific [Esri File GeoDatabase](https://pro.arcgis.com/en/pro-app/latest/help/data/geodatabases/manage-file-gdb/file-geodatabases.htm) into layers within a new [OGC GeoPackage](https://www.geopackage.org). This package is designed to be used from the command line or imported as a module.
+`fgdb_to_gpkg` is a Python package that converts all feature classes within a specific [Esri File GeoDatabase](https://pro.arcgis.com/en/pro-app/latest/help/data/geodatabases/manage-file-gdb/file-geodatabases.htm) into layers within a [OGC GeoPackage](https://www.geopackage.org). If the GeoPackage does not already exist, then a new one will be created. This package is designed to be used from the command line or imported as a module.
 
 This package does not have a dependency on ArcPy, which means that you can safely extract feature classes locked inside an Esri File GeoDataBase without needing to worry about any ArcGIS licensing.
 
@@ -38,4 +38,6 @@ input_fgdb_path = "/path/to/my_fgdb.gdb"
 output_gpkg_path = "/path/to/my_gpkg.gpkg"
 
 fgdb_to_gpkg(input_fgdb_path, output_gpkg_path, verbose=True)
+
+# See help(fgdb_to_gpkg) for more details
 ```
--- fgdb_to_gpkg.py
+++ fgdb_to_gpkg.py
@@ -1,45 +1,72 @@
 import argparse
 import geopandas as gpd
 import fiona
+import os
+from tqdm import tqdm
 
 
-def fgdb_to_gpkg(fgdb_path, gpkg_path, verbose=False):
+def fgdb_to_gpkg(fgdb_path, gpkg_path, overwrite=True, **kwargs):
     """Converts all feature classes within a File GeoDataBase to new layers within a GeoPackage.
 
     :param fgdb_path: file path of an Esri File GeoDataBase (.gdb)
     :type fgdb_path: str
 
-    :param gpkg_path: file path of a GeoPackage (.gpkg)
+    :param gpkg_path: file path of an OGC GeoPackage (.gpkg)
     :type fgdb_path: str
 
-    :param verbose: prints name of each feature class if True, defaults to False
+    :param overwrite: overwrites existing GeoPackage before copying over new layers if True, defaults to True
     :type bool, optional
+
+    :param **kwargs: additional keyword arguments to pass to geopandas.to_file()
     """
 
-    # List all feature classes within File GeoDataBase
-    fc_list = fiona.listlayers(fgdb_path)
+    try: 
 
-    # Loop through each feature class
-    for fc in fc_list:
-        if verbose:
-            print(fc)
+        # Handle relative file paths
+        fgdb_path = os.path.abspath(fgdb_path)
+        gpkg_path = os.path.abspath(gpkg_path)
 
-        # Read the feature class into GeoDataFrame
-        gdf = gpd.read_file(fgdb_path, layer=fc)
+        # Ensure input File GeoDataBase exists
+        if not os.path.exists(fgdb_path):
+            raise ValueError(f"{fgdb_path} does not exist!")
 
-        # Write the GeoDataFrame to a GeoPackage
-        gdf.to_file(gpkg_path, driver="GPKG", layer=fc, index=False, if_exists="append")
+        # Remove existing GeoPackage if overwrite is True
+        if os.path.exists(gpkg_path) and overwrite:
+            os.remove(gpkg_path)
 
+        # List all feature classes within File GeoDataBase
+        fc_list = fiona.listlayers(fgdb_path)
+
+        # Create progress bar
+        progress_bar = tqdm(total=len(fc_list), desc="Converting layers")
+
+        # Loop through each feature class
+        for fc in fc_list:
+
+            # Read the feature class into GeoDataFrame
+            gdf = gpd.read_file(fgdb_path, layer=fc)
+
+            # Write the GeoDataFrame to a GeoPackage
+            gdf.to_file(gpkg_path, driver="GPKG", layer=fc, index=False, if_exists="append", **kwargs)
+
+            # Update progress bar
+            progress_bar.update(1)
+
+        # Close progress bar
+        progress_bar.close()
+
+    except Exception as e:
+        print(f"Error converting {fgdb_path} to {gpkg_path}: {e}")
 
 if __name__ == '__main__':
     # Set up argparse to parse command line arguments
     parser = argparse.ArgumentParser(description='Convert an Esri File GeoDatabase to a GeoPackage')
     parser.add_argument('fgdb_path', type=str, help='path to the File GeoDatabase')
     parser.add_argument('gpkg_path', type=str, help='path to the GeoPackage to create')
-    parser.add_argument('--verbose', action='store_true', help='print the names of each feature class being converted')
+    parser.add_argument('--overwrite', action='store_true', help='deletes an existing GeoPackage before copying layers from File GeoDataBase.')
 
     # Parse command line arguments
     args = parser.parse_args()
 
     # Call the fgdb_to_gpkg function with the provided arguments
-    fgdb_to_gpkg(args.fgdb_path, args.gpkg_path, args.verbose)
+    fgdb_to_gpkg(args.fgdb_path, args.gpkg_path, args.overwrite)
